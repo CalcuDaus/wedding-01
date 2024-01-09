23452d0dd75d3d02e37559109361318cdf58656c
daus <dausg878@gmail.com>
2024-01-10 00:28:42 +0700

    tes

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,19 @@ const main = ()=>{
         coverEl.classList.replace('h-screen','h-0');
     })
     AOS.init();
+
+    // const setHeight = ()=>{
+    //     const currentHeight = window.innerHeight;
+    //     document.body.style.height = `${currentHeight}px`;
+    // };
+    // window.addEventListener('resize',setHeight);
+    // setHeight();
+    // const setWidth = ()=>{
+    //     const currentWidth = window.innerWidth;
+    //     document.body.style.width = `${currentWidth}px`;
+    // };
+    // window.addEventListener('resize',setWidth);
+    // setWidth();
 }
 
 document.addEventListener('DOMContentLoaded',main);
